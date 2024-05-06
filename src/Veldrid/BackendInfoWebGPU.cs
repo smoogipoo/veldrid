0b19518c5fc4129269777a890c5faeb74cf0290c
Dan Balasescu <smoogipoo@smgi.me>
2024-05-06 20:06:15 +0900

    Improve backend info

--- src/Veldrid/BackendInfoWebGPU.cs
+++ src/Veldrid/BackendInfoWebGPU.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Runtime.InteropServices;
 using Silk.NET.WebGPU;
 using Veldrid.WGPU;
 
@@ -9,13 +11,26 @@ namespace Veldrid
     public class BackendInfoWebGPU
     {
         public readonly Limits Limits;
+        public readonly string VendorName;
+        public readonly string DriverDescription;
+        public readonly string AdapterName;
+        public readonly AdapterType AdapterType;
+        public readonly BackendType BackendType;
 
         internal unsafe BackendInfoWebGPU(WGPUGraphicsDevice gd)
         {
             SupportedLimits deviceLimits = default;
             gd.WebGPU.DeviceGetLimits(gd.NativeDevice, ref deviceLimits);
 
+            AdapterProperties adapterProperties = default;
+            gd.WebGPU.AdapterGetProperties(gd.NativeAdapter, ref adapterProperties);
+
             Limits = deviceLimits.Limits;
+            VendorName = Marshal.PtrToStringUTF8((IntPtr)adapterProperties.VendorName);
+            DriverDescription = Marshal.PtrToStringUTF8((IntPtr)adapterProperties.DriverDescription);
+            AdapterName = Marshal.PtrToStringUTF8((IntPtr)adapterProperties.Name);
+            AdapterType = adapterProperties.AdapterType;
+            BackendType = adapterProperties.BackendType;
         }
     }
 }
